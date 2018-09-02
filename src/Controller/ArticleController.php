7fcb4d0867298c0cf4ed50dc710ef436dcce60d3
Fabian <fabianrupin@hotmail.com>
2018-09-02 17:50:06 +0200

    modify css and html for homepage

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomepageController extends AbstractController
+class ArticleController extends AbstractController
 {
     /**
-     * @Route("/homepage", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function index()
     {
-        return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+        return $this->render('article/index.html.twig', [
+            'controller_name' => 'ArticleController',
         ]);
     }
 }
