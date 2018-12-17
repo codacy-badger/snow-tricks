ffdf3ad3dc23279dc2e739b1c00648bb519fcb61
Fabian <fabianrupin@hotmail.com>
2018-12-17 08:58:28 +0100

    add paginate function in TrickRepo

--- src/Repository/TrickRepository.php
+++ src/Repository/TrickRepository.php
@@ -3,7 +3,10 @@
 namespace App\Repository;
 
 use App\Model\Entity\Trick;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use mysql_xdevapi\Exception;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * @method Trick|null find($id, $lockMode = null, $lockVersion = null)
@@ -16,6 +19,23 @@ class TrickRepository extends BaseRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Trick::class);
+    }
+
+    public function findAllSortAndPaginate($page, $maxTricksPerPage)
+    {
+        $qb = $this->createQueryBuilder('trick')
+            ->orderBy('trick.createdAt', 'DESC');
+
+        $query = $qb->getQuery();
+
+        $firstResult = ($page - 1) * $maxTricksPerPage;
+        $query->setFirstResult($firstResult)->setMaxResults($maxTricksPerPage);
+        $paginator = new Paginator($query);
+
+        if (($paginator->count() <= $firstResult) && $page != 1) {
+            throw new NotFoundHttpException();
+        }
+
     }
 
 //    /**
