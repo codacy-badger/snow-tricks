8b3ed9ecfa656c3aafbe265fdfc5407679ea4c30
Fabian <fabianrupin@hotmail.com>
2019-03-01 09:03:33 +0100

    change get controller response

--- src/Controller/Comment/GetController.php
+++ src/Controller/Comment/GetController.php
@@ -4,12 +4,13 @@ namespace App\Controller\Comment;
 
 
 use App\Repository\CommentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class GetController
+class GetController extends AbstractController
 {
     const NUMBER_OF_COMMENTS_PER_LOAD = 5;
 
@@ -38,11 +39,16 @@ class GetController
             self::NUMBER_OF_COMMENTS_PER_LOAD
         );
 
-        $response = new Response();
+        return $this->render('comments/__show.html.twig', [
+            'comments' => $comments,
+            'index' => $index,
+        ]);
+
+/*        $response = new Response();
 
         $response->headers->set('Content-Type', 'application/json');
         $response->setContent(json_encode($comments));
 
-        return $response;
+        return $response;*/
     }
 }
